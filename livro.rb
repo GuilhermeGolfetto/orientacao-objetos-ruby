35a0c2eb35a10e5f04995c9032e346444d42d3ae
desenvolvimento <desenvolvimento>
2016-06-16 16:42:24 -0300

    fim da primeira aula

--- livro.rb
+++ livro.rb
@@ -1,26 +1,29 @@
 class Livro
-	attr_reader :titulo, :ano_lancamento,:preco
-	def initialize(titulo,preco,ano_lancamento)
+	attr_reader :titulo, :ano_lancamento, :preco, :possui_reimpressao
+	def initialize(titulo,preco,ano_lancamento, possui_reimpressao)
 		@titulo = titulo
 		@ano_lancamento = ano_lancamento
 		@preco = calcula_preco(preco)
+		@possui_reimpressao = possui_reimpressao
 	end
 
-	private 
-
-	def calcula_preco(base)		
+	private def calcula_preco(base)		
 		if(@ano_lancamento < 2000)
 			base *= 0.7
 		else
 			base
 		end
 	end
+
+	def possui_reimpressao?
+		return @possui_reimpressao
+	end
 end
 
-livro_rails = Livro.new("Agile Web Development with Rails",70.00,2011)
+livro_rails = Livro.new("Agile Web Development with Rails",70.00,2011, true)
 
 
-livro_ruby = Livro.new("Programming Ruby 1.9",60.00,2010)
+livro_ruby = Livro.new("Programming Ruby 1.9",60.00,2010, true)
 
 livros = [livro_rails, livro_ruby]
 
@@ -35,8 +38,9 @@ def livro_para_newsletter(livro)
 		puts "Newsletter/liquidacao"
 		puts livro.titulo 
 		puts livro.preco
+		puts livro.possui_reimpressao?
 	end
 end
 
-algoritmos = Livro.new("Algoritmos",100,1998)
+algoritmos = Livro.new("Algoritmos",100,1998,true)
 livro_para_newsletter(algoritmos)
